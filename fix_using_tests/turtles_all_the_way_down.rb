73bcfd3e8efb84fdba00340c86e54ac22fe0bb66
Zeshan Raja <zraja4402@gmail.com>
2019-12-19 02:21:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -1,19 +1,17 @@
-require "pry"
+
 def turtles
     [
       {name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
-      {name: "Michaelangelo", weapon: "nunchuks", traits: ["party dude"]},
+      {name: "Michaelangelo", weapon: "nunchuks", traits: ["party dude"},
       {name: "Raphael", weapon: "sais", traits: ["cool", "rude"]},
       {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}
     ]
 end
 
 def turtle_traits(turtles)
-turtle_counter = 0 
-
-  while turtle_counter < turtles.count do 
-  result = turtles[turtle_counter][:traits]
-   turtle_counter += 1 
-  end 
-result
+  turtles.map do |turtle|
+    turtle[:traits].each do |trait|
+      trait
+    end
+  end
 end
